906be6f488600c38aa0b463ea11cc61712ff0dde
Admin <admin@Admins-MacBook-Air-2.local>
2025-02-14 13:24:40 +0530

    .

--- app/auth.py
+++ app/auth.py
@@ -25,19 +25,6 @@ def create_access_token(data: dict, expires_delta: timedelta = None):
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
-# def verify_access_token(token: str):
-#     try:
-#         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-#         return payload  
-#     except JWTError:
-#         return None
-
-# def verify_access_token(token: str):
-    # try:
-    #     payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-    #     return payload  
-    # except JWTError:
-    #     raise HTTPException(status_code=401, detail="Invalid or expired token")
 def verify_access_token(token: str):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
